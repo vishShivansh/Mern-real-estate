e2326a65b03747b1bd34f01729e669a001609eb7
Shivansh <sgshivansh22@gmail.com>
2024-04-11 11:24:02 +0530

    create update user API route

--- api/index.js
+++ api/index.js
@@ -1,3 +1,4 @@
+import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
 import express from "express";
 import mongoose from "mongoose";
@@ -17,6 +18,7 @@ mongoose
 const app = express();
 
 app.use(express.json());
+app.use(cookieParser());
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000!");
