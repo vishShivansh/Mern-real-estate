d575d23d6b1e91174d5501c0e3ccf6f05cc1ddc2
Shivansh <sgshivansh22@gmail.com>
2024-04-23 15:22:38 +0530

    create update listing API route

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -27,3 +27,24 @@ export const deleteListing = async (req, res, next) => {
     next(error);
   }
 };
+
+export const updateListing = async (req, res, next) => {
+  const listing = await Listing.findById(req.params.id);
+  if (!listing) {
+    return next(errorHandler(404, "Listing not found!"));
+  }
+  if (req.user.id !== listing.userRef) {
+    return next(errorHandler(401, "You can only update your own listing!"));
+  }
+  try {
+    const updatedlisting = await Listing.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true }
+    );
+
+    res.status(200).json(updatedlisting);
+  } catch (error) {
+    next(error);
+  }
+};
