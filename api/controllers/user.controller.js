ffb8a9ae5dfad488f4fc265ee4992cc62ab13155
Shivansh <sgshivansh22@gmail.com>
2024-05-24 15:15:42 +0530

    error handling in getUser route

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -63,8 +63,12 @@ export const getUserListings = async (req, res, next) => {
 };
 
 export const getUser = async (req, res, next) => {
-  const user = await User.findById(req.params.id);
-  if (!user) return next(errorHandler(404, "User not found!"));
-  const { password: pass, ...rest } = user._doc;
-  res.status(200).json(rest);
+  try {
+    const user = await User.findById(req.params.id);
+    if (!user) return next(errorHandler(404, "User not found!"));
+    const { password: pass, ...rest } = user._doc;
+    res.status(200).json(rest);
+  } catch (error) {
+    next(error);
+  }
 };
