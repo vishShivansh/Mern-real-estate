2882383a03812b4ffd728a20363e1b41f46f9514
vishShivansh <shivanshgtb@gmail.com>
2024-08-12 04:05:45 +0530

    -

--- backend/index.js
+++ backend/index.js
@@ -3,10 +3,11 @@ import cors from "cors";
 import dotenv from "dotenv";
 import express from "express";
 import mongoose from "mongoose";
-// import path from "path";
+import path from "path";
 import authRouter from "./api/routes/auth.route.js";
 import listingRouter from "./api/routes/listing.route.js";
 import userRouter from "./api/routes/user.route.js";
+
 dotenv.config();
 
 mongoose
@@ -18,7 +19,6 @@ mongoose
     console.log(err);
   });
 
-// const _dirname = path.resolve();
 const app = express();
 
 app.use(express.json());
@@ -32,19 +32,15 @@ const corsOptions = {
 
 app.use(cors(corsOptions));
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000!");
-});
-
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
 app.use("/api/listing", listingRouter);
 
-// app.use(express.static(path.join(_dirname, "/client/dist")));
+app.use(express.static(path.join(__dirname, "client", "dist")));
 
-// app.get("*", (req, res) => {
-//   res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
-// });
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
@@ -54,4 +50,9 @@ app.use((err, req, res, next) => {
     statusCode,
     message,
   });
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}!`);
 });
