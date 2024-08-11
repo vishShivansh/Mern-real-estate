5f0f4430bd7f6724bd07d8450eca62dfa1578151
vishShivansh <shivanshgtb@gmail.com>
2024-08-12 04:00:26 +0530

    -

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,21 +3,11 @@ import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/",
   server: {
     proxy: {
-      "/": {
-        target: "https://mern-real-estate-tkho.onrender.com/",
+      "/api": {
+        target: "http://localhost:3000",
         secure: false,
-        changeOrigin: true,
-      },
-    },
-  },
-  build: {
-    outDir: "dist", // This should match the Output Directory setting in Vercel
-    rollupOptions: {
-      input: {
-        main: "./index.html",
       },
     },
   },
