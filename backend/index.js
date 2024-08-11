a1f1b475e50a93e80b8d58f1bd2d338f3fc96502
vishShivansh <shivanshgtb@gmail.com>
2024-08-12 05:26:49 +0530

    updated vercel url(Frontend deployment)

--- backend/index.js
+++ backend/index.js
@@ -33,7 +33,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 const corsOptions = {
-  origin: "https://mern-real-estate-teal.vercel.app/",
+  origin: "https://mern-real-estate-gamma.vercel.app/",
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"],
 };
