d77cd06700bbbe7e788aea623e3e683860543adb
vishShivansh <shivanshgtb@gmail.com>
2024-08-11 22:48:55 +0530

    test

--- api/index.js
+++ api/index.js
@@ -2,23 +2,23 @@ import cookieParser from "cookie-parser";
 import cors from "cors";
 import dotenv from "dotenv";
 import express from "express";
-import mongoose from "mongoose";
-import path from "path";
+// import mongoose from "mongoose";
+// import path from "path";
 import authRouter from "./routes/auth.route.js";
 import listingRouter from "./routes/listing.route.js";
 import userRouter from "./routes/user.route.js";
 dotenv.config();
 
-mongoose
-  .connect(process.env.MONGO)
-  .then(() => {
-    console.log("Connected to MongoDB!");
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+// mongoose
+//   .connect(process.env.MONGO)
+//   .then(() => {
+//     console.log("Connected to MongoDB!");
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
 
-const _dirname = path.resolve();
+// const _dirname = path.resolve();
 const app = express();
 
 app.use(express.json());
@@ -33,19 +33,19 @@ app.use(
   })
 );
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000!");
-});
+// app.listen(3000, () => {
+//   console.log("Server is running on port 3000!");
+// });
 
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
 app.use("/api/listing", listingRouter);
 
-app.use(express.static(path.join(_dirname, "/client/dist")));
+// app.use(express.static(path.join(_dirname, "/client/dist")));
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
-});
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+// });
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
