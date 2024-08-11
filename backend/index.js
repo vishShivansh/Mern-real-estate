4af6c9886a7cb52930a23147f5b45bdc43854d99
vishShivansh <shivanshgtb@gmail.com>
2024-08-12 01:04:52 +0530

    moves out index.js from api

--- backend/index.js
+++ backend/index.js
@@ -3,9 +3,9 @@ import dotenv from "dotenv";
 import express from "express";
 import mongoose from "mongoose";
 import path from "path";
-import authRouter from "./routes/auth.route.js";
-import listingRouter from "./routes/listing.route.js";
-import userRouter from "./routes/user.route.js";
+import authRouter from "./api/routes/auth.route.js";
+import listingRouter from "./api/routes/listing.route.js";
+import userRouter from "./api/routes/user.route.js";
 dotenv.config();
 
 mongoose
