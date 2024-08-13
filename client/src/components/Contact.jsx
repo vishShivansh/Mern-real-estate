9c34edc6226cad83bdde56cbd3fefc3192b1c542
vishShivansh <shivanshgtb@gmail.com>
2024-08-13 22:34:46 +0530

    change in contact form fetch

--- client/src/components/Contact.jsx
+++ client/src/components/Contact.jsx
@@ -1,63 +1,5 @@
 /* eslint-disable no-undef */
 /* eslint-disable react/prop-types */
-// import { useEffect, useState } from "react";
-// import { Link } from "react-router-dom";
-
-// const API_BASE_URL = "https://mern-real-estate-a5fc.onrender.com";
-
-// export default function Contact({ listing }) {
-//   const [landlord, setLandlord] = useState(null);
-//   const [message, setMessage] = useState("");
-//   const onChange = (e) => {
-//     setMessage(e.target.value);
-//   };
-
-//   useEffect(() => {
-//     const fetchLandlord = async () => {
-//       try {
-//         const res = await fetch(`${API_BASE_URL}/api/user/${listing.userRef}`);
-//         const data = await res.json();
-//         console.log({ data });
-//         setLandlord(data);
-//       } catch (error) {
-//         console.log(error);
-//       }
-//     };
-//     fetchLandlord();
-//   }, [listing.userRef]);
-//   return (
-//     <>
-//       {landlord && (
-//         <div className="flex flex-col gap-2">
-//           <p>
-//             Contact <span className="font-semibold">{landlord.username}</span>{" "}
-//             for{" "}
-//             <span className="font-semibold">{listing.name.toLowerCase()}</span>
-//           </p>
-//           <textarea
-//             name="message"
-//             id="message"
-//             rows="2"
-//             value={message}
-//             onChange={onChange}
-//             placeholder="Enter your message here..."
-//             className="w-full border p-3 rounded-lg"
-//           ></textarea>
-
-//           <Link
-//             to={`mailto:${landlord.email}?subject=Regarding ${listing.name}&body=${message}`}
-//             className="bg-slate-700 text-white text-center p-3 uppercase rounded-lg hover:opacity-95"
-//           >
-//             Send Message
-//           </Link>
-//         </div>
-//       )}
-//     </>
-//   );
-// }
-
-/* eslint-disable no-undef */
-/* eslint-disable react/prop-types */
 import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
@@ -75,7 +17,10 @@ export default function Contact({ listing }) {
   useEffect(() => {
     const fetchLandlord = async () => {
       try {
-        const res = await fetch(`${API_BASE_URL}/api/user/${listing.userRef}`);
+        const res = await fetch(`${API_BASE_URL}/api/user/${listing.userRef}`, {
+          method: "GET",
+          credentials: "include",
+        });
         const data = await res.json();
 
         setLandlord(data);
