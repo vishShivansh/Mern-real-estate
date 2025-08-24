1a560e1abc3c6d8439b7d46117ebab9e7b109097
Shivansh <shivanshgtb@gmail.com>
2025-08-25 00:36:53 +0530

    update in vite config

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,12 +2,15 @@ import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
+
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+
 export default defineConfig({
   base: "/",
   server: {
     proxy: {
       "/api": {
-        target: "https://mern-real-estate-tkho.onrender.com", // Your backend URL
+        target: API_BASE_URL, // Your backend URL
         // target: "http://localhost:3000",
         changeOrigin: true,
       },
