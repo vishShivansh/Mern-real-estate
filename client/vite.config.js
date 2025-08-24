60129d318da8623c53dbfb8f1f5e532a2dc2912f
Shivansh <shivanshgtb@gmail.com>
2025-08-25 00:39:34 +0530

    update vite config

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,14 +3,13 @@ import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 export default defineConfig({
   base: "/",
   server: {
     proxy: {
       "/api": {
-        target: API_BASE_URL, // Your backend URL
+        target: "https://mern-real-estate-a5fc.onrender.com", // Your backend URL
         // target: "http://localhost:3000",
         changeOrigin: true,
       },
